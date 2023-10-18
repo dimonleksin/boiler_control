98b0a16bfad802a7462f1cb3d5d49934a2a0e773
0142 <leksindmitri@gmail.com>
2023-10-18 20:25:58 +0400

    Fix

--- alarm/temp_alarm.py
+++ alarm/temp_alarm.py
@@ -13,7 +13,7 @@ bot = telebot.TeleBot(os.getenv(alarm_settings.myToken))
 while True:
     # Alarm, when temp boiler 
     try:
-        resultGetTemp = requests.get(f"{alarm_settings.main_url}/gettemp")
+        resultGetTemp = json.load(requests.get(f"{alarm_settings.main_url}/gettemp").text)
         tempBoiler = resultGetTemp["tempBoiler"]
         print(f"Geted temp {tempBoiler}")
         if tempBoiler > alarm_settings.boiler_temp_alar: #or resultGetTemp["tempHouse"] < alarm_settings.home_temp_alarm:            
