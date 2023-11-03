a4165b6cd7d254d68b7478d162c81470be517895
0142 <leksindmitri@gmail.com>
2023-11-03 21:08:05 +0400

    fix

--- infra/new_ip.sh
+++ infra/new_ip.sh
@@ -2,7 +2,7 @@
 old_ip=$(cat /www/curent_ip)
 current_ip=$(curl ifconfig.me/ip)
 if [[ $old_ip != $current_ip ]]; then
-curl "https://api.telegram.org/bot6906656172:AAEngHkpw32TIsKlGWQXFh-BpJAfxonrrH8/sendMessage?chat_id=1776929080&text=$current_ip"
+curl "https://api.telegram.org/bot${TELEGRAMM_TOKEN}/sendMessage?chat_id=1776929080&text=$current_ip"
 echo "send new ip"
 else
 echo "ip not different"
